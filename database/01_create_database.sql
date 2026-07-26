8360850c9d32cd030a6c536e649dd9bdc9a3224e
Laukik Deshmukh <deshmukhlaukik52@gmail.com>
2026-07-26 14:39:51 +0530

    Create initial MySQL database

---
+++ database/01_create_database.sql
@@ -0,0 +1,9 @@
+-- Drop the database if it already exists (useful during development)
+DROP DATABASE IF EXISTS online_book_store;
+
+-- Create the database
+CREATE DATABASE online_book_store;
+
+-- Select the database for use
+USE online_book_store;
+
